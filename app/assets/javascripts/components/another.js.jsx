d0243601341a8b11ef5a64c6f0e8b01b77aceef3
Johannus V <johannus.vogel@gmail.com>
2016-07-22 12:22:46 +0200

    First commit, experimenting with React

---
+++ app/assets/javascripts/components/another.js.jsx
@@ -0,0 +1,12 @@
+var Another = React.createClass({
+
+
+render: function(){
+
+return(
+<div> Second Component andddd <br/>
+  {this.props.title}
+</div>
+);
+}
+});
---
+++ app/assets/javascripts/components/helloworld.js.jsx
@@ -0,0 +1,13 @@
+var Helloworld = React.createClass({
+
+
+render: function(){
+
+return(
+<div> My name is Johannus
+  <Another title={this.props.something}> </Another>
+
+</div>
+);
+}
+});
